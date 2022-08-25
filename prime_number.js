63f9b94ae57c7148f15dc7427e874091189fc04b
Anik Kumar Nath (Web) <108430535+aknathweb@users.noreply.github.com>
2022-08-25 23:55:21 +0600

    prime number check first way

--- prime_number.js
+++ prime_number.js
@@ -1,1 +1,13 @@
 // ২. কোন একটা সংখ্যা প্রাইম সংখ্যা (prime number) কিনা। সেটা চেক করার একটা ফাংশন লিখো। 
+
+function isPrime(number) {
+    const sqrt_number = Math.sqrt(number);
+    if (number == 1)
+        return false;
+    for (let i = 2; i <= sqrt_number; i++)
+        if (number % i == 0)
+            return false;
+
+    return true;
+}
+console.log(isPrime(4));
